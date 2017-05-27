9b85751b492c76e952cece5cea3cb4254236269c
Lev <lev@lev-hp-elitebook-850-g3.ericom.local>
2017-05-27 16:07:23 +0300

    Consul work in all modes that we need

---
+++ entry-point.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -x
+
+if [ -z "$CONSUL_DATA_DIR" ]; then
+    CONSUL_DATA_DIR=/consul/data
+    echo "Data dir configured to $CONSUL_DATA_DIR"
+fi
+
+if [ -z "$CONSUL_CONFIG_DIR" ]; then
+    CONSUL_CONFIG_DIR=/consul/config
+    echo "Config dir configured to $CONSUL_CONFIG_DIR"
+fi
+
+if [ -n "$CONSUL_BIND_INTERFACE" ]; then
+    CONSUL_BIND_ADDRESS=$(ip -o -4 addr list $CONSUL_BIND_INTERFACE | head -n1 | awk '{print $4}' | cut -d/ -f1)
+    echo "Right now consul bind address is"
+
+    CONSUL_BIND="-bind=$CONSUL_BIND_ADDRESS"
+fi
+
+consul agent \
+       $CONSUL_BIND \
+       -data-dir="$CONSUL_DATA_DIR" \
+       -config-dir="$CONSUL_CONFIG_DIR" \
+       -bootstrap-expect=3 \
+       -retry-join=consul \
+       -retry-interval=7s \
+       -server \
+       -ui \
+       -client=0.0.0.0 \
+       -disable-host-node-id=true
